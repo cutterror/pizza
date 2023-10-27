bda442bc485da58d2509d1e1c9d4305b182c109d
Anastasia <fioB7@yandex.ru>
2023-10-27 18:50:33 +0500

    nav, fonts added

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Header } from '../components/Header/Header';
+import { Main } from '../components/Main/Main';
+import { Navigation } from '../components/Navigation/Navigation';
 
 function App() {
     return (
         <div className="media">
             <Header/>
+            <Navigation/>
+            <Main/>
         </div>
     );
 }
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,32 @@
+import './Navigation.css';
+
+export function Navigation() {
+    return (
+        <nav>
+            <a href="#pizza">
+                Пицца
+            </a>
+            <a href="">
+                Комбо
+            </a>
+            <a href="">
+                Напитки
+            </a>
+            <a href="">
+                Десерты
+            </a>
+            <a href="">
+                Акции
+            </a>
+            <a href="/about">
+                О нас
+            </a>
+            <a href="/about">
+                Контакты
+            </a>
+            <a href="/game">
+                Игра
+            </a>
+        </nav>
+    );
+}
